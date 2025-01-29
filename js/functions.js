c79e54146b019edbbe85bd254f01314b90d0200d
Pandenimik <89628864+Pandenimik@users.noreply.github.com>
2025-01-29 09:53:02 +0300

    Вносит правки по замечаниям.

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ const validateStringLength = (validatingString, maxLength) => validatingString.l
 
 // Функция для проверки, является ли строка палиндромом.
 
-const isPalindrom = (originalString) => {
+const isPalindrome = (originalString) => {
   const cleanString = originalString.replaceAll(' ', '').toLowerCase();
   for (let i = 0; i <= cleanString.length / 2; i++) {
     if (cleanString.at(i) !== cleanString.at(-(i + 1))) {
@@ -18,23 +18,9 @@ const isPalindrom = (originalString) => {
 // и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры,
 // функция должна вернуть NaN.
 
-const getNumbers = (originalString) => {
-  const stringWithoutWhitespaces = originalString
-    .toString()
-    .replaceAll(' ', '');
-  let stringOfNumbers = '';
-  for (let i = 0; i < stringWithoutWhitespaces.length; i++) {
-    if (!isNaN(+stringWithoutWhitespaces[i])) {
-      stringOfNumbers += stringWithoutWhitespaces[i];
-    }
-  }
-  if (stringOfNumbers) {
-    return parseInt(stringOfNumbers, 10);
-  }
-  return NaN;
-};
+const getNumbers = (originalString) => parseInt((originalString.toString().replaceAll(/\D/g, '')), 10);
 
 // Вызов функций, для обхода ошибки eslint ('функция' is assigned a value but never used).
 validateStringLength('просто', 10);
-isPalindrom('Топот');
+isPalindrome('Топот');
 getNumbers(-1.5);
